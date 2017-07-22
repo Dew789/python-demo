1341900b1a0d80af47ee7bb34ca38c41f6569895
HangZhang <z86hang@163.com>
2017-07-22 16:12:36 +0800

    Add mergeSort

--- sort.py
+++ sort.py
@@ -48,8 +48,32 @@ def selectionSort(t):
         t[i], t[min_pos] = t[min_pos], t[i]
 
 # Merge Sort
-def mergeSort():
-    pass
+def mergeSort(t):
+    length = len(t)
+    if length == 1:
+        return t
+    mid = length // 2
+    left = mergeSort(t[:mid])
+    right = mergeSort(t[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    length_left = len(left)
+    lenght_right = len(right)
+    i, j = 0, 0
+    result = []
+    while i < length_left and j < lenght_right:
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    if i < length_left:
+        result.extend(left[i:])
+    else:
+        result.extend(right[j:])
+    return result
 
 # Bucket Sort
 def bucketSort():

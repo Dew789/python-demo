2ac8a7e30cecfea275f6f2c2a04dec77363b87ac
MaoWang233 <z86hang@163.com>
2017-07-29 19:52:00 +0800

    Add heap

--- sort.py
+++ sort.py
@@ -75,7 +75,7 @@ def merge(left, right):
         result.extend(right[j:])
     return result
 
-# Bucket Sort
+# Radix Sort
 import math
 
 def radixSort_LSD(t):
@@ -129,8 +129,24 @@ def quickSort(t, front, rear):
     
 
 # Heap Sort
-def Heap_sort():
-    pass
+def make_heap(t):
+    length = len(t)
+    minest_root = length // 2 - 1
+    for i in range(minest_root, -1 ,-1):
+        _sift_up(t, i, length)
+
+def _sift_up(t, root, end):
+    child = 2 * root + 1
+    temp = t[root]
+    while child < end:
+        if child + 1 < end and t[child + 1] < child:
+            child += 1
+        if t[child] > t[root]:
+            break
+        t[root] = t[child]
+        root = child
+        child = 2 * child + 1
+    t[root] = temp
 
 if __name__ == '__main__':
     a = [7, 8, 5, 1, 9, 2, 4, 6, 3, 10]

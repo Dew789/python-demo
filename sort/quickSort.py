83f3edc4333b442578bfa1188e03bb17f87c975b
Rick <z86hang@163.com>
2017-12-14 19:06:02 +0800

    randomized quick sort

--- sort/quickSort.py
+++ sort/quickSort.py
@@ -1,4 +1,6 @@
-def quickSort(t, front, rear):
+from random import randint
+
+def quick_sort(t, front, rear):
     if front >= rear:
         return
 
@@ -12,5 +14,47 @@ def quickSort(t, front, rear):
             l += 1
         t[r] = t[l]
     t[r] = key
-    quickSort(t, front, l-1)
-    quickSort(t, r+1, rear)
+    quick_sort(t, front, l-1)
+    quick_sort(t, r+1, rear)
+
+def randomized_quick_sort(t, front, rear):
+    """ 
+        if imput array is sorted, 
+        common quick sort will be O(n^2).
+        randomized the povit would make complexity to O(nlogn).
+    """
+    if front >= rear:
+        return
+    rand_index = randint(front, rear)
+    povit = t[rand_index]
+    i = front
+    for index in range(front, rear+1):
+        # Don't swap povit
+        if i == rand_index:
+            i += 1
+        if t[index] < povit:
+            temp = t[i]
+            t[i] = t[index]
+            t[index] = temp
+            i += 1
+    # Based on i to swap povit
+    if i > rand_index:
+        i -= 1
+    temp = t[i]
+    t[i] = povit
+    t[rand_index] = temp
+    randomized_quick_sort(t, front, i)
+    randomized_quick_sort(t, i+1, rear)
+    
+
+if __name__ == '__main__':
+    t = []
+    for i in range(10000000):
+        t.append(i)
+    from time import time
+    start = time()
+    randomized_quick_sort(t, 0, 99)
+    print(time() - start)
+    start = time()
+    quick_sort(t, 0, 99)
+    print(time()-start)

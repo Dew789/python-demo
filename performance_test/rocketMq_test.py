d451a2d4eb96c4b8a03fef7166574aab8df6acec
hangzhang2 <hangzhang2@iflytek.com>
2023-08-31 15:09:44 +0800

    修复rocketMQ测试代码

--- performance_test/rocketMq_test.py
+++ performance_test/rocketMq_test.py
@@ -5,22 +5,19 @@ import uuid
 from rocketmq.client import Producer, Message
 from concurrent.futures import ThreadPoolExecutor
 
-"""
-  wget https://github.com/apache/rocketmq-client-cpp/releases/download/2.0.0/rocketmq-client-cpp-2.0.0-centos7.x86_64.rpm
-  sudo rpm -ivh rocketmq-client-cpp-2.0.0-centos7.x86_64.rpm
-  find / -name librocketmq.so
-  export LD_LIBRARY_PATH=/usr/lib/librocketmq.so:$LD_LIBRARY_PATH
-  sudo ldconfig
-  """
-def send_message(url):
-    producer = Producer("rocketMQ-test-group")
-    producer.set_name_server_address(url)
-    producer.start()
 
+def send_message(producer):
+    """
+      wget https://github.com/apache/rocketmq-client-cpp/releases/download/2.0.0/rocketmq-client-cpp-2.0.0-centos7.x86_64.rpm
+      sudo rpm -ivh rocketmq-client-cpp-2.0.0-centos7.x86_64.rpm
+      find / -name librocketmq.so
+      export LD_LIBRARY_PATH=/usr/local/lib/librocketmq.so:$LD_LIBRARY_PATH
+      sudo ldconfig
+    """
     body = {
         "deviceSn": "cafe00000001:1",
         "messageId": str(uuid.uuid4()),
-        "msgTime": 1693065601916,
+        "msgTime": int(time.time() * 1000),
         "tcpData": {
             "bits": 16,
             "samplingRate": 48000,
@@ -40,8 +37,12 @@ def send_message(url):
 
 def concurrent_send(url, count):
     pool = ThreadPoolExecutor(count)
+    producer = Producer("rocketMQ-test-group")
+    producer.set_name_server_address(url)
+    producer.start()
+
     for i in range(1, count + 1):
-        pool.submit(send_message, url)
+        pool.submit(send_message, producer)
 
     while True:
         time.sleep(10)

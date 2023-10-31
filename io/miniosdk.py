8e7c1fe678b318961b7c529e7f5172359537cad3
hangzhang2 <hangzhang2@iflytek.com>
2023-10-31 11:02:55 +0800

    添加minio下载脚本

--- io/miniosdk.py
+++ io/miniosdk.py
@@ -1,22 +1,53 @@
+import os
 from minio import Minio
+from minio.error import S3Error
 
 minio_client = Minio(endpoint='10.40.152.222:19000',
                      access_key='****',
                      secret_key='*********',
                      secure=False)
 
-bucket_name = '*****'
-policy = """
-{
-    "Statement": 
-    [{"Action": 
-    ["s3:GetBucketLocation", "s3:ListBucket"],
-     "Effect": "Allow", "Principal": "*", "Resource": "arn:aws:s3:::%s"},
-     {"Action": "s3:GetObject", "Effect": "Allow", "Principal": "*", "Resource": "arn:aws:s3:::%s/*"}
-    ], 
-    "Version": "2012-10-17"}
-""" % (bucket_name, bucket_name)
+
+def set_policy(bucket_name):
+    policy = """
+    {
+        "Statement": 
+        [{"Action": 
+        ["s3:GetBucketLocation", "s3:ListBucket"],
+         "Effect": "Allow", "Principal": "*", "Resource": "arn:aws:s3:::%s"},
+         {"Action": "s3:GetObject", "Effect": "Allow", "Principal": "*", "Resource": "arn:aws:s3:::%s/*"}
+        ], 
+        "Version": "2012-10-17"}
+    """ % (bucket_name, bucket_name)
+
+    minio_client.set_bucket_policy(bucket_name, policy)
+
+
+def download_folder(bucket_name, folder_path, local_path):
+    """
+        # 替换为你的MinIO服务器地址、访问密钥、密钥和桶名
+    minio_server = "fengdian-minio.gyznts.com"
+    access_key = "minioadmin"
+    secret_key = "huOS9erJnXts"
+    bucket_name = "ark-media"
+
+    # 替换为你要下载的文件夹路径和本地存储路径
+    folder_path = "zhiting/783017485108322304_904448987690960896/1/202309/"
+    local_path = "/root/data"
+    """
+    if not os.path.exists(local_path):
+        os.makedirs(local_path)
+
+    for obj in minio_client.list_objects(bucket_name, prefix=folder_path):
+        file_name = obj.object_name
+        file_path = os.path.join(local_path, file_name)
+        try:
+            minio_client.fget_object(bucket_name, file_name, file_path)
+            print(f"下载成功： {file_name}")
+        except S3Error as e:
+            print(f"下载失败： {file_name}, 错误： {e}")
+
 
 if __name__ == "__main__":
-    minio_client.set_bucket_policy(bucket_name, policy)
+    pass
 
